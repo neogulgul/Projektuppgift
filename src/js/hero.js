bfccf13d3434e28942abb949182e3e64bdeded16
Neo Gullberg <neogullberg@tutanota.com>
2022-03-14 18:52:35 +0100

    interval slide change

--- src/js/hero.js
+++ src/js/hero.js
@@ -2,8 +2,8 @@ let leftArrow = document.querySelector(".arrow:first-of-type")
 let rightArrow = document.querySelector(".arrow:last-of-type")
 let dotsContainer = document.querySelector("#dots")
 let dots = document.querySelector("#dots").children
-let heroImagesContainer = document.querySelector("#hero-images")
-let heroImages = document.querySelector("#hero-images").children
+let heroImagesContainer = document.querySelector("#hero-slides")
+let heroImages = document.querySelector("#hero-slides").children
 
 // creating dots
 for (i = 0; i < heroImages.length; i++) {
@@ -26,48 +26,41 @@ firstImageClone = heroImages[0].outerHTML
 heroImagesContainer.insertAdjacentHTML("afterbegin", lastImageClone)
 heroImagesContainer.insertAdjacentHTML("beforeend", firstImageClone)
 
-leftArrow.onclick = () => {
-    if (translation >= maxTranslation) return // prevents the slideshow from going out of sync
+function arrow(direction) {
+    slideTime = 0
+
+    if (direction === "left") {
+        if (translation >= maxTranslation) return // preventing the slideshow from going out of sync
+        translation += 100
+    } else if (direction === "right") {
+        if (translation <= minTranslation) return // preventing the slideshow from going out of sync
+        translation -= 100
+    }
+
+    // animation
+    heroImagesContainer.style.transition = "0.5s"
+    heroImagesContainer.style.transform = `translateX(${translation}vw)`
 
     // dots
     for (i = 0; i < dots.length; i++) {
         if (dots[i].classList.contains("fill")) {
             dots[i].classList.remove("fill")
-            if (i === 0) {
-                dots[dots.length - 1].classList.add("fill")
-            } else {
-                dots[i - 1].classList.add("fill")
+            if (direction === "left") {
+                if (i === 0) {
+                    dots[dots.length - 1].classList.add("fill")
+                } else {
+                    dots[i - 1].classList.add("fill")
+                }
+            } else if (direction === "right") {
+                if (i === dots.length - 1) {
+                    dots[0].classList.add("fill")
+                } else {
+                    dots[i + 1].classList.add("fill")
+                }
             }
             break
         }
     }
-
-    // animation
-    translation += 100
-    heroImagesContainer.style.transition = "0.3s"
-    heroImagesContainer.style.transform = `translateX(${translation}vw)`
-}
-
-rightArrow.onclick = () => {
-    if (translation <= minTranslation) return // prevents the slideshow from going out of sync
-
-    // dots
-    for (i = 0; i < dots.length; i++) {
-        if (dots[i].classList.contains("fill")) {
-            dots[i].classList.remove("fill")
-            if (i === dots.length - 1) {
-                dots[0].classList.add("fill")
-            } else {
-                dots[i + 1].classList.add("fill")
-            }
-            break
-        }
-    }
-
-    // animation
-    translation -= 100
-    heroImagesContainer.style.transition = "0.3s"
-    heroImagesContainer.style.transform = `translateX(${translation}vw)`
 }
 
 heroImagesContainer.addEventListener("transitionend", () => {
@@ -80,3 +73,22 @@ heroImagesContainer.addEventListener("transitionend", () => {
         heroImagesContainer.style.transform = `translateX(${translation}vw)`
     }
 })
+
+leftArrow.onclick = () => {
+    arrow("left")
+}
+
+rightArrow.onclick = () => {
+    arrow("right")
+}
+
+let slideTime = 0
+let changeSlide = 10 // seconds
+
+setInterval(function() {
+    slideTime += 1
+    console.log(slideTime) // remove this later
+    if (slideTime === changeSlide) {
+        arrow("right")
+    }
+}, 1000)
