80056cc371d0ec35de29f71e2d10427680c8fb3e
Neo Gullberg <neogullberg04@gmail.com>
2022-03-16 15:59:14 +0100

    list of products

--- src/js/products.js
+++ src/js/products.js
@@ -1,3 +1,70 @@
+const products = [
+    // CPUs
+    {
+        name: "",
+        image: "",
+        brand: "",
+        component: "cpu",
+        price: 0
+    },
+    // CPU Coolers
+    {
+        name: "",
+        image: "",
+        brand: "",
+        component: "",
+        price: 0
+    },
+    // Motherboards
+    {
+        name: "",
+        image: "",
+        brand: "",
+        component: "",
+        price: 0
+    },
+    // Memory
+    {
+        name: "",
+        image: "",
+        brand: "",
+        component: "",
+        price: 0
+    },
+    // Storage
+    {
+        name: "",
+        image: "",
+        brand: "",
+        component: "",
+        price: 0
+    },
+    // Graphics cards
+    {
+        name: "",
+        image: "",
+        brand: "",
+        component: "",
+        price: 0
+    },
+    // Power Supplies
+    {
+        name: "",
+        image: "",
+        brand: "",
+        component: "",
+        price: 0
+    },
+    // Cases
+    {
+        name: "",
+        image: "",
+        brand: "",
+        component: "",
+        price: 0
+    },
+]
+
 let component = window.location.pathname.split("/src/components/")[1].split(".html")[0]
 
 let main = document.querySelector("main")
