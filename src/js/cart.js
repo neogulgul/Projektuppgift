1b99aa0b877612daf3857403fd661460b385b65d
Neo Gullberg <neogullberg@tutanota.com>
2022-03-26 16:20:15 +0100

    re-did cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,104 +1,109 @@
-const tableBody = document.querySelector("tbody")
-const subTotalMarkup = document.querySelector("#checkout span")
+let totalPrice = 0
 
-let subTotal = 0
+function loadItems() {
+	Object.keys(localStorage).forEach((item) => {
+		let product = products[item]
+		let quantity = parseInt(localStorage.getItem(item))
+		let price = product.price * quantity
 
-Object.keys(localStorage).forEach((item) => {
-    let product = products[item]
+		totalPrice += price
 
-    let quantity = parseInt(localStorage.getItem(item))
-    let price = product.price * quantity
+		let itemRow = `
+		<tr id="${product.id}">
+			<td>
+				<div class="product-column">
+					<img src="images/products/${product.component}/${product.image}">
+					<aside>
+						<p>${product.name}</p>
+						<p>$${product.price}</p>
+						<button class="remove">Remove</button>
+					</aside>
+				</div>
+			</td>
+			<td>
+				<div class="quantity-column">
+					<div class="arrows">
+						<svg class="up" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 12"><path d="M 0,12 L 24,12 L12,0 L 0,12"/></svg>
+						<svg class="down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 12"><path d="M 0,0 L 24,0 L12,12 L 0,0"/></svg>
+					</div>
+					<p>${quantity}</p>
+				</div>
+			</td>
+			<td>
+				<div class="subtotal-column">$${price}</div>
+			</td>
+		</tr>`
 
-    subTotal += price
-
-    let tableRow = `
-    <tr id="${product.id}">
-        <td>
-            <div>
-                <img src="images/products/${product.component}/${product.image}">
-                <div class="name-and-price">
-                    <p>${product.name}</p>
-                    <p>$${product.price}</p>
-                    <button class="remove">Remove</button>
-                </div>
-            </div>
-        </td>
-        <td>
-            <div class="quantity">
-                <div class="arrows">
-                    <svg class="up" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 12"><path d="M 0,12 L 24,12 L12,0 L 0,12"/></svg>
-                    <svg class="down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 12"><path d="M 0,0 L 24,0 L12,12 L 0,0"/></svg>
-                </div>
-                <p>${quantity}</p>
-            </div>
-        </td>
-        <td class="price-total">$${price}</td>
-    </tr>`
-
-    tableBody.innerHTML += tableRow
-})
-
-updateSubTotal()
-
-function updateSubTotal() {
-    subTotalMarkup.innerText = "$" + subTotal
+		document.querySelector("tbody").innerHTML += itemRow
+	})
 }
+
+function updateTotal() {
+	document.querySelector("#checkout span").innerText = "$" + totalPrice
+}
+
+function cartDisplay() {
+	if (getCartQuantity() === 0) {
+		document.querySelector("main").innerHTML += "<h2>Your cart is currently empty.</h2>"
+		document.querySelector("table").style.display = "none"
+	}
+}
+
+loadItems()
+updateTotal()
+cartDisplay()
 
 let arrows = document.querySelectorAll(".arrows svg")
 
 arrows.forEach((arrow) => {
-    arrow.onmousedown = (event) => {
-        let targetElement = event.target
-        if (event.target.tagName === "path") {
-            targetElement = event.target.parentElement
-        }
+	arrow.onmousedown = (event) => {
+		if (leftClick(event)) {
+			let itemRow = arrow.parentElement.parentElement.parentElement.parentElement
+			let itemId = itemRow.id
+			let item = products[parseInt(itemId)]
+			let quantity = parseInt(localStorage.getItem(itemId))
+			let subTotalContainer = itemRow.getElementsByClassName("subtotal-column")[0]
 
-        let itemRow = targetElement.parentElement.parentElement.parentElement.parentElement
-        let itemId = itemRow.id
-        let item = products[parseInt(itemId)]
-        let quantity = parseInt(localStorage.getItem(itemId))
-        let priceTotal = itemRow.children[2]
+			if (arrow.classList.contains("up")) {
+				quantity += 1
+				totalPrice += item.price
+			} else if (arrow.classList.contains("down")) {
+				quantity -= 1
+				totalPrice -= item.price
+			}
 
-        if (targetElement.classList.contains("up")) {
-            quantity += 1
-            priceTotal.innerText = "$" + item.price * quantity
-            subTotal += item.price
-        } else if (targetElement.classList.contains("down")) {
-            quantity -= 1
-            priceTotal.innerText = "$" + item.price * quantity
-            subTotal -= item.price
+			if (quantity > 0) {
+				localStorage.setItem(itemId, quantity)
+				subTotalContainer.innerText = "$" + item.price * quantity
+				arrow.parentElement.parentElement.children[1].innerText = quantity
+			} else {
+				localStorage.removeItem(itemId)
+				itemRow.remove()
+				cartDisplay()
+			}
 
-            if (quantity === 0) {
-                localStorage.removeItem(itemId)
-                itemRow.remove()
-            }
-        }
-
-        if (quantity > 0) {
-            localStorage.setItem(itemId, quantity)
-            targetElement.parentElement.parentElement.children[1].innerText = `${localStorage.getItem(itemId)}`
-        }
-
-        updateSubTotal()
-        updateCart()
-    }
+			updateCart()
+			updateTotal()
+		}
+	}
 })
 
 let removeButtons = document.querySelectorAll(".remove")
 
 removeButtons.forEach((button) => {
-    button.onclick = (event) => {
-        let itemRow = event.target.parentElement.parentElement.parentElement.parentElement
-        let itemId = itemRow.id
-        let item = products[parseInt(itemId)]
-        let quantity = parseInt(localStorage.getItem(itemId))
+	button.onclick = () => {
+		let itemRow = button.parentElement.parentElement.parentElement.parentElement
+		let itemId = itemRow.id
+		let item = products[parseInt(itemId)]
+		let quantity = parseInt(localStorage.getItem(itemId))
 
-        localStorage.removeItem(itemId)
-        itemRow.remove()
+		localStorage.removeItem(itemId)
+		itemRow.remove()
 
-        subTotal -= item.price * quantity
+		totalPrice -= item.price * quantity
 
-        updateSubTotal()
-        updateCart()
-    }
+		updateCart()
+		updateTotal()
+		cartDisplay()
+	}
 })
