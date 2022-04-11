1b30e6d9d7758217509fb54a35280e779687879b
neo <>
2022-04-11 15:20:02 +0200

    site wrapper to account for mobile overflow

--- src/js/components.js
+++ src/js/components.js
@@ -3,7 +3,7 @@ const component = window.location.pathname.split("/src/components/")[1].split(".
 const star = `<svg class="star" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 18.26l-7.053 3.948 1.575-7.928L.587 8.792l8.027-.952L12 .5l3.386 7.34 8.027.952-5.935 5.488 1.575 7.928z"/></svg>`
 const starEmpty = `<svg class="star empty" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 18.26l-7.053 3.948 1.575-7.928L.587 8.792l8.027-.952L12 .5l3.386 7.34 8.027.952-5.935 5.488 1.575 7.928z"/></svg>`
 
-function addToProductsMain() {
+function createTable() {
     let a = "a"
 
     if (component === "memory" || component === "storage") {
@@ -71,7 +71,7 @@ function createProducts() {
     })
 }
 
-addToProductsMain()
+createTable()
 createProducts()
 
 let buttons = document.querySelectorAll("tr button")
